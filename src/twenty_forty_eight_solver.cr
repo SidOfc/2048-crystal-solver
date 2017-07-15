1e186349ca99cf0b502662050b294992f440c4f2
Sidney Liebrand <sidneyliebrand@gmail.com>
2017-07-15 15:14:06 +0200

    Tools and sample algo for solving the game

--- src/twenty_forty_eight_solver.cr
+++ src/twenty_forty_eight_solver.cr
@@ -1,10 +1,34 @@
 require "./twenty_forty_eight_solver/*"
 require "twenty_forty_eight"
+require "twenty_forty_eight/options"
+require "colorize"
 
 module TwentyFortyEightSolver
   extend self
 
-  def entropy(board)
+  # lower entropy is better
+  def evaluate(board)
+    directions = available board
+
+    return nil unless directions.any?
+
+    Array({Symbol, Float32}).new(directions.size) do |idx|
+      direction = directions[idx]
+      {direction, weight merge_in direction, board}
+    end.sort { |a, b| a[1] <=> b[1] }.first.first
+  end
+
+  def merge_in(direction, board)
+    case direction
+    when :right then right board
+    when :down  then down board
+    when :left  then left board
+    when :up    then up board
+    else board
+    end
+  end
+
+  def weight(board)
     smoothness   = 0
     monotonicity = 0
     total        = 0
@@ -60,43 +84,130 @@ module TwentyFortyEightSolver
     # worst possible state when the board is not aligned.
     return Float32::INFINITY unless edges.find &.==({ max_loc[:x], max_loc[:y] })
 
-    tmp = [smoothness, monotonicity, (size ** 2), (max_tile ** 4), total].map &.to_f
+    tmp = [smoothness, monotonicity, (size ** 2), (max_tile ** 4), total].map &.to_f32
     sum = tmp[0] + tmp[1] + tmp[2] - tmp[3] - tmp[4]
 
     sum / tmp.max
   end
+
+  def up(board)
+    left(board.transpose).transpose
+  end
+
+  def down(board)
+    right(board.transpose).transpose
+  end
+
+  def left(board)
+    board.map { |row| merge row }
+  end
+
+  def right(board)
+    board.map { |row| merge(row.reverse).reverse }
+  end
+
+  def merge(row)
+    tmp, res = row - [0], Array(Int32).new
+
+    while cur = tmp.shift?
+      cmp = tmp.shift?
+
+      if cur == cmp
+        mrg     = cur << 1
+        res    << mrg
+      else
+        res << cur
+        break unless cmp
+        tmp.unshift cmp
+      end
+    end
+
+    res.concat Array(Int32).new(row.size - res.size) { 0 }
+  end
+
+  def available(board)
+    out = [] of Symbol
+    out << :up if up? board
+    out << :down if down? board
+    out << :right if right? board
+    out << :left if left? board
+    out
+  end
+
+  def up?(board)
+    board != up board
+  end
+
+  def down?(board)
+    board != down board
+  end
+
+  def left?(board)
+    board != left board
+  end
+
+  def right?(board)
+    board != right board
+  end
 end
 
-board = [[0, 0, 0, 0],
-         [8, 4, 4, 2],
-         [1024, 64, 512, 128],
-         [2048, 1024, 512, 256]]
+# tilevalue => [:fore, :back]
+COLOR_MAP = {
+  0    => [:white, :white],
+  2    => [:white, :light_green],
+  4    => [:white, :green],
+  8    => [:white, :light_yellow],
+  16   => [:white, :light_red],
+  32   => [:white, :red],
+  64   => [:white, :light_magenta],
+  128  => [:white, :magenta],
+  256  => [:white, :light_cyan],
+  512  => [:white, :cyan],
+  1024 => [:white, :light_blue],
+  2048 => [:white, :blue]
+}
 
-board2 = [[32, 8, 16, 64],
-          [128, 512, 32, 16],
-          [32, 8, 16, 512],
-          [2048, 512, 8, 16]]
+def render(board, padding = 9)
+  board.map do |row|
+    row.map do |value|
+      fg, bg = COLOR_MAP[value]
+      strval = value == 0 ? "" : value.to_s
+      rem    = (padding - strval.size) / 2.0
+      filler = (" " * padding).colorize.back(bg).to_s
+      fmttd  = ((" " * rem.floor.to_i) +
+                strval.colorize.fore(fg).bold.to_s +
+                (" " * rem.ceil.to_i).colorize.back(bg).to_s).colorize.back(bg).to_s
 
-# board = board.transpose
-
-board.each do |row|
-  puts row.join '|'
+      [filler, fmttd, filler]
+    end.transpose.map(&.join).join "\n"
+  end.join "\n"
 end
 
-puts "entropy: #{TwentyFortyEightSolver.entropy board}"
+mode = :smart
 
-board2.each do |row|
-  puts row.join '|'
+loop do
+  TwentyFortyEight.sample(TwentyFortyEight.options.size) do
+    sleep 0.2
+    puts render board
+    case mode
+    when :smart then move TwentyFortyEightSolver.evaluate board
+    when :naive then down || left || right || up
+    end
+    move = TwentyFortyEightSolver.evaluate board
+
+    move ||= down || left || right || up
+
+    puts "\033[#{(board.size * 3) + 1}A"
+    move
+  end
 end
 
-puts "entropy: #{TwentyFortyEightSolver.entropy board2}"
-
-
-# puts "*" * 60
-
-# board2.each do |row|
-#   puts row.join '|'
+# TwentyFortyEight.sample do
+#   sleep 0.5
+#   puts render board
+#   best = TwentyFortyEightSolver.evaluate(board)
+#   break unless best[0]? && best[0][0]?
+#   puts "\033[#{(board.size * 3) + 1}A"
+#   move best[0][0]
 # end
 
-# puts "emptiness: #{TwentyFortyEightSolver.emptiness board2}"
-# puts "emptiness: #{TwentyFortyEightSolver.monotonicity board2}"
